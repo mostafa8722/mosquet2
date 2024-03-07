c10eeb13c666c251ba46a26ba39cc333ca2d3f28
mostafa8722 <mostafa.8722@gmail.com>
2024-03-07 19:17:11 +0330

    DEBUG: add session js

--- src/pages/api/session.js
+++ src/pages/api/session.js
@@ -1,9 +1,8 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./auth/[...nextauth]";
 
-import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req, res) {
     const session = await getServerSession(req, res, authOptions);
     res.send(JSON.stringify(session, null, 2));
 }
